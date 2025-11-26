65ea5cd605139d1320feea4933ccc2f075683fe9
barma <barmatey@gmail.com>
2025-11-26 17:34:22 +0300

    init

--- src/spoofer/usecases.rs
+++ src/spoofer/usecases.rs
@@ -1,22 +1,19 @@
-use crate::level2::{BookStats, OrderBook};
+use crate::level2::{OrderBook};
 use crate::spoofer::SpooferDetected;
 use crate::trade::TradeStats;
 
 pub struct FindSpoofers<'a> {
     order_book: &'a OrderBook,
-    book_stats: &'a BookStats,
     trade_stats: &'a TradeStats,
 }
 
 impl<'a> FindSpoofers<'a> {
     pub fn new(
         order_book: &'a OrderBook,
-        book_stats: &'a BookStats,
         trade_stats: &'a TradeStats,
     ) -> Self {
         Self {
             order_book,
-            book_stats,
             trade_stats,
         }
     }
