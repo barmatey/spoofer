f2990f35145effbcb263b6c61013e93fc6402d86
barma <barmatey@gmail.com>
2025-11-26 17:34:01 +0300

    init

--- src/level2/mod.rs
+++ src/level2/mod.rs
@@ -1,4 +1,3 @@
-mod book_stats;
 mod events;
 mod order_book;
 mod traits;
@@ -7,7 +6,5 @@ mod errors;
 pub use order_book::{OrderBook};
 
 pub use events::LevelUpdated;
-
-pub use book_stats::{BookStats, Snap};
 
 pub use errors::Level2Error;
