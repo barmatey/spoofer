62297ac00581f52c430008e0897999e5402e8f2b
barma <barmatey@gmail.com>
2025-11-29 13:27:08 +0300

    init

--- src/connector/mod.rs
+++ src/connector/mod.rs
@@ -1,6 +1,8 @@
 mod connector;
 mod connector_binance;
 mod errors;
+mod connector_conibase;
 
 pub use connector::Connector;
 pub use connector_binance::{BinanceConnector, BinanceConnectorConfig};
+pub use connector_conibase::{CoinbaseConnector, CoinbaseConnectorConfig};
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use crate::connector::{CoinbaseConnector, CoinbaseConnectorConfig, Connector};
+use crate::shared::Bus;
+
 mod connector;
 mod shared;
 mod signal;
@@ -7,5 +10,30 @@ mod trade;
 
 #[tokio::main]
 async fn main() {
-    println!("Bye!")
+    let bus = Bus::new();
+
+    let config = CoinbaseConnectorConfig{
+        product_id: "BTC-USD".to_string(),
+        price_multiply: 100,
+        quantity_multiply: 100_000,
+    };
+
+
+    let printer = tokio::spawn(async move {
+        loop {
+            let events = bus.trades.pull();
+            for ev in events {
+                println!("{:?}", ev);
+            }
+            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        }
+    });
+
+    let listener = tokio::spawn(async move {
+        let mut connector = CoinbaseConnector::new(&bus, config);
+        connector.listen().await;
+    });
+
+    let _ = tokio::join!(printer, listener);
+
 }
