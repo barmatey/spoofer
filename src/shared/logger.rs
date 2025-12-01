e9debd716b422679f51d1587067ed21753bb25ab
barma <barmatey@gmail.com>
2025-12-01 21:43:28 +0300

    init

---
+++ src/shared/logger.rs
@@ -0,0 +1,17 @@
+pub struct Logger {
+    name: &'static str,
+}
+
+impl Logger {
+    pub fn new(name: &'static str) -> Self {
+        Self { name }
+    }
+
+    pub fn info(&self, msg: &str) {
+        tracing::info!(exchange = self.name, "{msg}");
+    }
+
+    pub fn error(&self, err: &str) {
+        tracing::error!(exchange = self.name, "{err}");
+    }
+}
--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -1,6 +1,8 @@
 mod bus;
 mod types;
 pub mod datetime;
+mod logger;
 
 pub use bus::Bus;
 pub use types::{Price, Quantity, TimestampMS, Side, Period,};
+pub use logger::Logger;
