e753545aa7f23259fbb1d26b30e0159cd4407b6a
barma <barmatey@gmail.com>
2025-12-01 22:27:15 +0300

    init

--- src/connector/services/parser.rs
+++ src/connector/services/parser.rs
@@ -1,15 +1,15 @@
-use crate::connector::errors::ParsingError::MessageParsingError;
+use crate::connector::errors::ParsingError::{ConvertingError, MessageParsingError, SerdeError};
 use crate::connector::errors::{Error, ParsingError};
 use crate::shared::TimestampMS;
 use chrono::{DateTime, Utc};
 use serde_json::{Map, Value};
 
 pub fn get_serde_value(raw: &str) -> Result<Value, Error> {
-    let result = serde_json::from_str::<Value>(raw).map_err(|e| {
-        eprintln!("[kraken] JSON parse error: {:?}, raw: {}", e, raw);
-        MessageParsingError(format!("JSON parse error: {:?}", e))
-    })?;
-    Ok(result)
+    let result = serde_json::from_str::<Value>(raw);
+    match result { 
+        Ok(r) => Ok(r),
+        Err(err) => Err(SerdeError(err))?,
+    }
 }
 
 pub fn get_serde_object(raw: &str) -> Result<Map<String, Value>, Error> {
@@ -25,7 +25,7 @@ pub fn parse_json<T: serde::de::DeserializeOwned>(s: &str) -> Result<T, Error> {
     let result = serde_json::from_str::<T>(s);
     match result {
         Ok(r) => Ok(r),
-        Err(e) => Err(ParsingError::SerdeError(e))?,
+        Err(e) => Err(SerdeError(e))?,
     }
 }
 
@@ -35,7 +35,7 @@ pub fn parse_value<T: serde::de::DeserializeOwned>(
     let result = serde_json::from_value::<T>(value);
     match result {
         Ok(r) => Ok(r),
-        Err(e) => Err(ParsingError::SerdeError(e))?,
+        Err(e) => Err(SerdeError(e))?,
     }
 }
 
@@ -43,18 +43,18 @@ pub fn parse_number(s: &str) -> Result<f64, ParsingError> {
     let result = serde_json::from_str::<f64>(s);
     match result {
         Ok(r) => Ok(r),
-        Err(e) => Err(ParsingError::ConvertingError(format!("{}", e))),
+        Err(e) => Err(SerdeError(e)),
     }
 }
 
 pub fn parse_timestamp(s: &str) -> Result<TimestampMS, ParsingError> {
     s.parse::<TimestampMS>()
-        .map_err(|e| ParsingError::ConvertingError(format!("{}", e)))
+        .map_err(|e| ConvertingError(format!("{}", e)))
 }
 
 pub fn parse_timestamp_from_date_string(s: &str) -> Result<TimestampMS, ParsingError> {
     let dt = DateTime::parse_from_rfc3339(s)
-        .map_err(|e| ParsingError::ConvertingError(format!("Failed to parse datetime: {}", e)))?;
+        .map_err(|e| ConvertingError(format!("Failed to parse datetime: {}", e)))?;
     let timestamp_ms = dt.with_timezone(&Utc).timestamp_millis();
     Ok(timestamp_ms as TimestampMS)
 }
