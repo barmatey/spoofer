aafb9371e2526fc3cb39fc446a1dff7d785f9e49
barma <barmatey@gmail.com>
2025-12-07 12:13:05 +0300

    processing

--- src/shared/exchange.rs
+++ src/shared/exchange.rs
@@ -1,5 +1,14 @@
 #[derive(PartialEq, Clone, Debug)]
-pub enum Exchange{
+pub enum Exchange {
     Binance = 0,
     Kraken = 1,
+}
+
+impl Exchange {
+    pub fn to_str(&self) -> &'static str {
+        match self {
+            Exchange::Binance => "binance",
+            Exchange::Kraken => "kraken",
+        }
+    }
 }
